d0dac7c7580696c05f4cdb76556680a3e1a01979
Jay Wu <wu274319426@gmail.com>
2017-06-22 15:42:40 -0400

    RandomSome

---
+++ src/randomSomeDebug/RandomSome.java
@@ -0,0 +1,24 @@
+package randomSomeDebug;
+import java.util.*;
+public class RandomSome {
+	   public static void main(String[] args)
+	    {
+	        output("Generate 10 random integers between 0 and 6");
+	       
+	        Random rnd = new Random();
+	      
+	        
+	        for (int i = 1; i <= 10; ++i)
+	        {
+	          int randomInt = rnd.nextInt(6);
+	          output("Generated number: " + randomInt);
+	        }
+	    
+	        output("Done.");
+	    }
+	  
+	  private static void output(String aMessage)
+	  {
+	    System.out.println(aMessage);
+	  }
+	}
